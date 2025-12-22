4cb265b3dc3bdd65ef856e2c6546eb9d24e62ad4
Samuel Mwangi <crystalflow.mwangi@gmail.com>
2025-12-22 15:15:49 +0300

    Add bindings for http::method::Method.

---
+++ src/request/method.rs
@@ -0,0 +1,85 @@
+use hyper::http::method::Method as LibMethod;
+use napi::bindgen_prelude::*;
+use napi_derive::napi;
+
+#[napi]
+pub struct Method {
+  inner: LibMethod,
+}
+
+impl From<Method> for LibMethod {
+  fn from(value: Method) -> Self {
+    value.inner.clone()
+  }
+}
+
+impl From<LibMethod> for Method {
+  fn from(value: LibMethod) -> Self {
+    Self { inner: value }
+  }
+}
+
+#[napi]
+impl Method {
+  #[napi(constructor)]
+  pub fn connect() -> Self {
+    Self::from(LibMethod::CONNECT)
+  }
+
+  #[napi(constructor)]
+  pub fn delete() -> Self {
+    Self::from(LibMethod::DELETE)
+  }
+
+  #[napi(constructor)]
+  pub fn get() -> Self {
+    Self::from(LibMethod::GET)
+  }
+
+  #[napi(constructor)]
+  pub fn head() -> Self {
+    Self::from(LibMethod::HEAD)
+  }
+
+  #[napi(constructor)]
+  pub fn options() -> Self {
+    Self::from(LibMethod::OPTIONS)
+  }
+
+  #[napi(constructor)]
+  pub fn patch() -> Self {
+    Self::from(LibMethod::PATCH)
+  }
+
+  #[napi(constructor)]
+  pub fn post() -> Self {
+    Self::from(LibMethod::POST)
+  }
+
+  #[napi(constructor)]
+  pub fn put() -> Self {
+    Self::from(LibMethod::PUT)
+  }
+
+  #[napi(constructor)]
+  pub fn trace() -> Self {
+    Self::from(LibMethod::TRACE)
+  }
+
+  #[napi(constructor)]
+  pub fn from_bytes(src: Uint8Array) -> Result<Self> {
+    LibMethod::from_bytes(src.as_ref())
+      .map(Self::from)
+      .map_err(|e| Error::new(Status::InvalidArg, e.to_string()))
+  }
+
+  #[napi]
+  pub fn is_indempotent(&mut self) -> bool {
+    self.inner.is_idempotent()
+  }
+
+  #[napi]
+  pub fn to_string(&mut self) -> String {
+    self.inner.to_string()
+  }
+}

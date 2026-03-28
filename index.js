f031878db7f579df8bbef50a98302051f734598b
Samuel Mwangi <crystalflow.mwangi@gmail.com>
2026-03-28 18:06:22 +0300

    Update package version to 0.*.* as API is not reliabel.

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = require("./hyperjs-core/index");
 // interface ServerListenOptions {
-//     backlog: number,
-//     exclusive: boolean,
-//     host: string,
-//     ipv6Only: boolean,
-//     reusePort: boolean,
-//     path: string,
-//     port: number,
-//     readableAll: boolean,
-//     signal: AbortSignal,
-//     writableAll: boolean
+//   backlog: number
+//   exclusive: boolean
+//   host: string
+//   ipv6Only: boolean
+//   reusePort: boolean
+//   path: string
+//   port: number
+//   readableAll: boolean
+//   signal: AbortSignal
+//   writableAll: boolean
 // }
 // class TestServer {
-//   listen(handle, backlog: number, callback);
-//   listen(options: ServerListenOptions, callback);
-//   listen(path: string, backlog: number, callback);
-//   listen(port: number, host: string, backlog: number, callback);
+//   listen(handle, backlog: number, callback)
+//   listen(options: ServerListenOptions, callback)
+//   listen(path: string, backlog: number, callback)
+//   listen(port: number, host: string, backlog: number, callback)
 //   listen() {}
 // }
 function hyperjs() {
--- index.ts
+++ index.ts
@@ -1,26 +1,25 @@
 import { Server, Request, Response } from './hyperjs-core/index'
 
 // interface ServerListenOptions {
-//     backlog: number,
-//     exclusive: boolean,
-//     host: string,
-//     ipv6Only: boolean,
-//     reusePort: boolean,
-//     path: string,
-//     port: number,
-//     readableAll: boolean,
-//     signal: AbortSignal,
-//     writableAll: boolean
+//   backlog: number
+//   exclusive: boolean
+//   host: string
+//   ipv6Only: boolean
+//   reusePort: boolean
+//   path: string
+//   port: number
+//   readableAll: boolean
+//   signal: AbortSignal
+//   writableAll: boolean
 // }
 
 // class TestServer {
-//   listen(handle, backlog: number, callback);
-//   listen(options: ServerListenOptions, callback);
-//   listen(path: string, backlog: number, callback);
-//   listen(port: number, host: string, backlog: number, callback);
+//   listen(handle, backlog: number, callback)
+//   listen(options: ServerListenOptions, callback)
+//   listen(path: string, backlog: number, callback)
+//   listen(port: number, host: string, backlog: number, callback)
 
 //   listen() {}
-
 // }
 
 function hyperjs() {
